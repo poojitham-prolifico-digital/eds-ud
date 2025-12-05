d6159de53e72bc3efbdd7bc3cc69d659b8cc9436
Poojitha Malapati <poojitham_prolifico.digital@wrenchpartner.com>
2025-12-05 17:38:12 +0530

    writing js matching with eds give numbers

--- blocks/herobanner/herobanner.js
+++ blocks/herobanner/herobanner.js
@@ -45,13 +45,13 @@ export default function decorate(block) {
 
 
 <div class="banner-content__cta">          
-                <div class="cta button a-button a-button--primary a-button--md a-link a-link--icon a-button--icon-left" data-aue-prop="ctaLink" data-aue-type="aem-link >${ctaLink}${ctaLabel}1
+                <div class="cta button a-button a-button--primary a-button--md a-link a-link--icon a-button--icon-left" data-aue-prop="ctaLink" data-aue-type="aem-link >
 
 <div>
 
-	<button tabindex="0" class="btn   schedule-engine-integration-cta   " aria-label="Schedule now" id="button-bea9cd5877"  >${ctaLink}${ctaLabel}2
+	<button tabindex="0" class="btn   schedule-engine-integration-cta   " aria-label="Schedule now" id="button-bea9cd5877"  >
 
-		<em class="wg-icon wg-icon-calendar"></em>${ctaLabel}${ctaLink}3
+		<em class="wg-icon wg-icon-calendar"></em>${ctaLabel}${ctaLink}
 	</button>
 </div>
 
